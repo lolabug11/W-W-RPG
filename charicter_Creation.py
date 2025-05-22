5482b70f96599a4b0dc99300725d91fe2f8233b5
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2025-05-22 16:53:58 -0400

    Update charicter_Creation.py

--- charicter_Creation.py
+++ charicter_Creation.py
@@ -8,16 +8,29 @@ class Charicter:
         return 
 def  charicter_creation():
     Class = input('What class do you want to be out of this list\nBarbarian\nDruid\nEnter your desision: ')
-    player = Charicter(50,None,None ,Class)
-    
+    player = Charicter(50 ,None,None , Class)
+    print(player.Class)
+    weapon = None
     if Class.lower() == 'barbarian':
         weapon = 'Great Sword'
-    if Class.lower() ==  'druid':
-        weapon = 'scimitar'
+    elif Class.lower() ==  'druid':
+        weapon = 'fang dagger'
     print(f'you now have a {weapon}')
-    print(f'you roll for starting gold. You roll a {roll_dice(20)}')
-if __name__ == '__main__':
-    Player = Charicter(50,None,None,'Barbarian')
-    print(Player.Class)
-    if Player.Class == 'Barbarian':
-        print('yes')
+    return player
+    #
+def starting_gold(charicter_class):
+    if charicter_class == 'druid' or 'barbarian':
+        roll  = roll_dice(4) + roll_dice(4)
+        amount = roll * 10
+    if charicter_class == 'wizard' or 'warlock' or 'rouge':
+        roll = roll_dice(4) + roll_dice(4) + roll_dice(4) + roll_dice(4) 
+        amount = roll * 10
+
+    print(f'you roll for starting gold. You roll a {roll} you get {amount} GP')
+    return int(amount)
+
+
+
+
+if __name__ == '__main__':  
+    charicter_creation()

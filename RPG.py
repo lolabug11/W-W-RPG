648f18f0fedae00dc1ffeb6df53f313f9f7fad89
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2025-05-22 16:53:36 -0400

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,8 +1,31 @@
 from charicter_Creation import *
 from damage_system import * 
+#tell the mean of the data
+roll = None
 def main():
-    charicter_creation()
+    player = charicter_creation()
+    i = 0
+    l = 0
+    h = 0
+    total = 0
+    while i < 100000000:
+        amount = starting_gold(player.Class)
+        total += amount
+        
+
+        if amount > h:
+            h =  amount
+        elif amount < l:
+            l = amount
+        i += 1
+    mean = total / 100000000
+    print(mean)
+    
+
+
+     
 if __name__ == '__main__':
     main()
+
 
 
